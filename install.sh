e1287a52275964e3967dca386e83d6165f186d7e
NikaKereselidze <71851989+NikaKereselidze@users.noreply.github.com>
2021-11-11 14:21:37 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,3 @@ echo 'Installing BeautifulSoup library'
 sudo pip3 install bs4
 echo 'Installing requests library'
 sudo pip3 install requests
-echo 'Installing the lxml parser'
-sudo pip3 install lxml
